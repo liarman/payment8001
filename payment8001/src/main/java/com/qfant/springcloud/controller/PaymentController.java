9d530176700065ae254da62a838c0e926d3a36e0
chenghui <287288189@qq.com>
2021-03-11 18:35:10 +0800

    .

--- payment8001/src/main/java/com/qfant/springcloud/controller/PaymentController.java
+++ payment8001/src/main/java/com/qfant/springcloud/controller/PaymentController.java
@@ -5,9 +5,12 @@ import com.qfant.springcloud.entities.Payment;
 import com.qfant.springcloud.service.PaymentService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.client.ServiceInstance;
+import org.springframework.cloud.client.discovery.DiscoveryClient;
 import org.springframework.web.bind.annotation.*;
 
 import javax.annotation.Resource;
+import java.util.List;
 
 /**
  * @Author: chenghui
@@ -20,20 +23,36 @@ public class PaymentController {
     private PaymentService paymentService;
     @Value("${server.port}")
     private String serverPort;
+    @Resource
+    private DiscoveryClient discoveryClient;
+
     @PostMapping(value = "/payment/create")
-    public CommonResult create(@RequestBody Payment payment){
-        int result= paymentService.create(payment);
-        log.info("插入结果:"+result);
-        if(result>0){
-            return new CommonResult(200,"插入成功，端口："+serverPort,result);
-        }else {
-            return new CommonResult(500,"插入失败",null);
+    public CommonResult create(@RequestBody Payment payment) {
+        int result = paymentService.create(payment);
+        log.info("插入结果:" + result);
+        if (result > 0) {
+            return new CommonResult(200, "插入成功，端口：" + serverPort, result);
+        } else {
+            return new CommonResult(500, "插入失败", null);
         }
     }
 
     @GetMapping(value = "/payment/get/{id}")
-    public CommonResult getPaymentById(@PathVariable("id")Integer id){
-        Payment result= paymentService.getPaymentById(id);
-        return new CommonResult(200,"查询成功，端口："+serverPort,result);
+    public CommonResult getPaymentById(@PathVariable("id") Integer id) {
+        Payment result = paymentService.getPaymentById(id);
+        return new CommonResult(200, "查询成功，端口：" + serverPort, result);
+    }
+
+    @GetMapping(value = "/payment/discoveryClient")
+    public Object discoveryClient() {
+        List<String> services = discoveryClient.getServices();
+        for (String s : services) {
+            System.out.println("*******"+s);
+        }
+        for (ServiceInstance instance : discoveryClient.getInstances("CLOUD-PAYMENT-SERVICE")) {
+            log.info("实例信息："+instance.getInstanceId()+"\t"+instance.getHost()+"\t"+instance.getPort()+"\t"+instance.getUri());
+        }
+
+        return this.discoveryClient;
     }
 }
